02a04709459f2ccd6442ca8a0efa815725a74d21
guido <guidofrassetti@gmail.com>
2025-09-21 21:01:01 -0300

    revert

--- api/src/routes/integrations.ts
+++ api/src/routes/integrations.ts
@@ -83,7 +83,7 @@ router.get("/connect/whatsapp", async (req: AuthRequest, res: Response) => {
     const authUrl = `https://www.facebook.com/v19.0/dialog/oauth?` +
       `client_id=${config.metaAppId}&` +
       `redirect_uri=${config.apiUrl}/integrations/oauth/whatsapp/callback&` +
-      `scope=public_profile&` +
+      `scope=whatsapp_business_management,whatsapp_business_messaging&` +
       `response_type=code&` +
       `state=${state}`;
 
